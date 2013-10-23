72104bab6e6068d3893745640d158f985b0e578d
Amey Sakhadeo <me@ameyms.com>
2013-10-23 14:18:15 +0530

    API docs for the Plotable interface (more to come)

--- src/com/symantec/gwt/flot/client/Plotable.java
+++ src/com/symantec/gwt/flot/client/Plotable.java
@@ -2,8 +2,20 @@ package com.symantec.gwt.flot.client;
 
 import com.google.gwt.user.client.ui.Widget;
 
+/**
+ * An interface that is implemented by the {@link Flot} base class
+ */
 public interface Plotable
 {
+	/**
+	 * Draws the Flot graph inside an element, replacing an existing content.<br/>
+	 * Classes implementing this method should ideally call the <code>$.plot()</code> here.
+	 * @param element The element inside which the graph needs to be drawn
+	 */
 	void plot(Widget element);
+	
+	/**
+	 * Destroys event handlers and other references to DOM.
+	 */
 	void cleanup();
 }
